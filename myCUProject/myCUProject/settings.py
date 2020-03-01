c5895f378d337264e4693ca67df3c9f0141f1618
citz73 <citz73@gmail.com>
2020-02-29 18:13:58 -0700

    psql database connected + created test model for our app database

--- myCUProject/myCUProject/settings.py
+++ myCUProject/myCUProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myCU_App', 
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'myCUProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myCUSide_Project',
+        'USER': 'chanlee', 
+        'HOST': '127.0.0.1', 
+        'PORT':' 5432'
     }
 }
 
