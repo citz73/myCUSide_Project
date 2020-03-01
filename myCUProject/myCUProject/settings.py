2e3fc9f1e9ccb9a527b7e5c6b8db8c6ea190b83a
citz73 <citz73@gmail.com>
2020-02-29 18:36:35 -0700

    password added to db

--- myCUProject/myCUProject/settings.py
+++ myCUProject/myCUProject/settings.py
@@ -79,6 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'myCUSide_Project',
         'USER': 'chanlee', 
+        'PASSWORD': 'secret',
         'HOST': '127.0.0.1', 
         'PORT':' 5432'
     }
