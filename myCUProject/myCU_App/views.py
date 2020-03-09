860a3aa93f72992746e5d0375d24c8b59e6deac4
citz73 <citz73@gmail.com>
2020-03-08 22:09:07 -0600

    data models inital set up and notification on login page

--- myCUProject/myCU_App/views.py
+++ myCUProject/myCU_App/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
+
+def user_profile(request, *args, **kwargs):
+    return render(request, "profile.html", {})
+
