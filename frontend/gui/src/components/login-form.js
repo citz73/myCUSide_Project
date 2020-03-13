a121c1b16948b12a305177bde514d34b5bd0eeff
Ryan Lansdon <ryanlansdon@gmail.com>
2020-03-12 21:47:31 -0600

    Fixed changes maybe?

---
+++ frontend/gui/src/components/login-form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+    return (
+        <React.Fragment>
+            <span>Title</span>
+            <input type="text" />
+            <span>Description</span>
+        </React.Fragment>
+    )
+}
+
+export default LoginForm;
