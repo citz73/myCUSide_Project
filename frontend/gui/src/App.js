f1cd3e2f9ce9ce728fcfec00ced87157178c9de2
Ryan Lansdon <ryanlansdon@gmail.com>
2020-03-12 21:47:31 -0600

    Fixed changes maybe?

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import HomeHeader from './components/home-header.js';
+//import LoginForm from './components/login-form.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <HomeHeader />
-        <h1>MyCUProject</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <HomeHeader />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
